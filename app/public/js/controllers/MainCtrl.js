9083e1f6df1f43b5d28b089d7d85a6c62a9584db
David Kim <dhkim1211@gmail.com>
2017-01-16 23:00:03 -0800

    hide nav in voting view, hide nav elements on login

--- app/public/js/controllers/MainCtrl.js
+++ app/public/js/controllers/MainCtrl.js
@@ -1,11 +1,21 @@
 angular.module('spotivote')
-    .controller('MainCtrl', ['$scope', '$http', '$location',
-        function($scope, $http, $location) {
+    .controller('MainCtrl', ['$scope', '$http', '$location', '$state',
+        function($scope, $http, $location, $state) {
             $scope.params = $location.path().split('/')[3] || "Unknown";
-            $scope.register = function() {
-                $http.post('/')
-            }
+
             $scope.verify = function() {
                 $http.post('/register/verify/' + $scope.params, $scope.data)
+            }
+
+            $scope.joinEvent = function(code) {
+                $state.go('vote', {id: code});
+            }
+
+            $scope.state = $state;
+
+            $scope.logout = function() {
+                $http.get('/auth/logout').success(function(data) {
+                    $state.go('home');
+                })
             }
         }]);
